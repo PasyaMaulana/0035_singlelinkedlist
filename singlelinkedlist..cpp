5687928c25a722184ba35178a9bfa26c96979c40
PasyaMaulana <pasyamaulana17@gmail.com>
2025-05-08 20:05:38 +0700

    case 1

--- singlelinkedlist..cpp
+++ singlelinkedlist..cpp
@@ -129,5 +129,13 @@ int main()
         cout << endl
              << "Masukkan pilihan (1-5): ";
         cin >> ch;
+        switch (ch)
+        {
+        case '1':
+        {
+            mhs.addNode();
+            break;
+        }
+        }
     }
 }
